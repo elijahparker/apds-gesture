a1dd88e170bc657a5e7a289230b8a970ae271926
Elijah Parker <elijahparker@gmail.com>
2016-02-16 10:44:23 -0600

    debugging

--- example/example.js
+++ example/example.js
@@ -14,9 +14,9 @@ gesture.debug = true;
 gesture.on('ready', function() {
     console.log("found a gesture sensor");
     gesture.setup(function() {
-        setInterval(function() {
-            gesture.readGesture();
-        }, 500);
+        //setInterval(function() {
+        gesture.readGesture();
+        //}, 500);
     });
 });
 
