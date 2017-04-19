30faa2854e4416311e9eb76dd8f2801934cca6ba
Elijah Parker <elijahparker@gmail.com>
2017-04-19 11:53:36 -0500

    bugfixes and GPIO addition

--- index.js
+++ index.js
@@ -652,7 +652,8 @@ GestureSensor.prototype.readGesture = function(testCallback) {
 
     if(self.gpio) {
         gpio.read(self.gpio, function(err, dataReady) {
-            if(dataReady == 0) {
+            console.log(dataReady);
+            if(dataReady) {
                 if(self.debug) console.log("GESTURE: data ready via GPIO");
                 dataFound();
             } else {
